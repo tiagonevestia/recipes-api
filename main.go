018d1e4d23d8069f467de1a2d3d22a4d22d38ed8
Tiago Neves <tiagoneves.tia@gmail.com>
2022-03-06 12:10:41 -0300

    feat: boilerplate

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Recipe struct {
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"published_at"`
+}
+
+func main() {
+	router := gin.Default()
+
+	router.Run()
+}
